5991aeae156b67ee8e41765b5650cecee03394f1
Yann <y.kassabji@gmail.com>
2018-11-12 16:53:43 +0100

    squelette-update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@
 
 $(function () {
       $('.burger').click(function () {
+      	var height = window.height;
             $(this).toggleClass('open');
             $('nav').toggleClass('open-nav', 1000, "easeOutSine" );
+            /*$('.open-nav').height(height);*/
       });
 });
