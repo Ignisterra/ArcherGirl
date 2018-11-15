6d42982b3289c82faff1bf9e698421e730c08748
Yann <y.kassabji@gmail.com>
2018-11-15 16:59:31 +0100

    firebase

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,3 +1,66 @@
+/****************** LEADER BOARD ********************/
+
+//envoi score et vérif
+
+ function sendScore(){
+
+	var score = $('#inputTest').val();
+	var name = $('#inputName').val();
+	var pos = $('#idlist').val();  
+	var tab = [];
+	var player = {
+			Pos : pos,
+		    Name: name,
+		    Score: score,		  
+			};
+	 firebase.database().ref('leaderBoard/').once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var item = childSnapshot.val();
+          tab[item.Pos] = item;
+        });
+        console.log(tab);
+      });         
+	 for (var i = tab.length, i >=0, i--){
+	 	if (score > tab[i].Score){
+	 		tab[i] = player;
+		 		
+			}
+	 	}
+
+	 	int i, j;
+	   for (i = 1; i < taille; ++i) {
+	       int elem = tab[i];
+	       for (j = i; j > 0 && tab[j-1] > elem; j--)
+	           tab[j] = tab[j-1];
+	       tab[j] = elem;
+	   }
+	/*var list = checkList();*/
+	firebase.database().ref('/leaderBoard/' + pos).set({
+			Pos : pos,
+		    Name: name,
+		    Score: score,		  
+	});
+	/*window.location.replace("leader_board.html");*/
+}
+
+// Affichage Leader Board 
+async function showList(){
+   
+        firebase.database().ref('leaderBoard/').once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot, i) {
+          var item = childSnapshot.val();
+          $('#lead').append('<li class="item">pos :'+(item.Pos+1)+' Nom : '+item.Name+'<span> Score : '+item.Score+'</span></li>');;
+        });
+      });         
+}
+
+
+/* $('#lead').append('<li class="item">Nom : '+item.Name+'<span> Score : '+item.Score+'</span></li>');
+
+/************* Authentification et controleur ****************/
+
+
+//authentification en anonyme
 function authAnonymous(){
 
 	firebase.auth().signInAnonymously().catch(function(error) {
@@ -8,6 +71,7 @@ function authAnonymous(){
 	});
 }
 
+// Controlleur
 function initApp() {
 
 	firebase.auth().onAuthStateChanged(function(user) {
@@ -20,12 +84,12 @@ function initApp() {
 	    // User is signed out.
 	    // ...
 	  }
-	  // ...
+	  showList();
 	});
-
+	$('#buttonTest').on('click',sendScore);
 }
 
 $(function() {
-		authAnonymous();
+	authAnonymous();
     initApp();
 });
